7af9be17502d76e895778070b73525fc98d1fe62
fantuanchaoren <124227255+fantuanchaoren@users.noreply.github.com>
2023-07-13 16:56:19 +0800

    Update GoogleAdsense.js

--- components/GoogleAdsense.js
+++ components/GoogleAdsense.js
@@ -27,19 +27,3 @@ export default function GoogleAdsense () {
   }, [router.events])
   return null
 }
-
-
-const AdSlot = ({ type = 'show' }) => {
-  if (!BLOG.ADSENSE_GOOGLE_ID) {
-    return null
-  }
-  // 文章内嵌广告
-  if (type === 'in-article') {
-    return <ins className="adsbygoogle"
-            style={{ display: 'block', textAlign: 'center' }}
-            data-ad-layout="in-article"
-            data-ad-format="fluid"
-            data-adtest={BLOG.ADSENSE_GOOGLE_TEST ? 'on' : 'off'}
-            data-ad-client={BLOG.ADSENSE_GOOGLE_ID}
-            data-ad-slot="4186549915"></ins>
-  }
