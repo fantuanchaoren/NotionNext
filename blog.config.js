d06466903703f21f2f17b00667272885e5746349
fantuanchaoren <124227255+fantuanchaoren@users.noreply.github.com>
2024-07-04 16:46:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '饭团超人', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '日语爱好者', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '漫威非官方数据库', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://panzian.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog, Marvel', // 网站关键词 英文逗号隔开
 
